7e9aeb5461bbb884eb7df7aa00264371f29376d2
asd <asd@asd-PC>
2015-12-28 22:11:25 +0300

    Mahmoud

---
+++ SchoolManagmentSystem/src/com/school/pojo/Manager.java
@@ -0,0 +1,193 @@
+package com.school.pojo;
+
+import java.util.Date;
+
+public class Manager {
+
+    private int manId;
+    private Date dateOfHire;
+    private String qualification;
+    private String position;
+    private String firstName;
+    private String midName;
+    private String lastName;
+    private String address;
+    private String gender;
+    private Date dateOfBirth;
+    private String phone;
+    private String type;
+
+    public static void main(String[] args) {
+        System.out.println("I 'm Manager...MahmoudTestNow");
+    }
+
+   
+
+    /**
+     * @return the dateOfHire
+     */
+    public Date getDateOfHire() {
+        return dateOfHire;
+    }
+
+    /**
+     * @param dateOfHire the dateOfHire to set
+     */
+    public void setDateOfHire(Date dateOfHire) {
+        this.dateOfHire = dateOfHire;
+    }
+
+    /**
+     * @return the qualification
+     */
+    public String getQualification() {
+        return qualification;
+    }
+
+    /**
+     * @param qualification the qualification to set
+     */
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    /**
+     * @return the position
+     */
+    public String getPosition() {
+        return position;
+    }
+
+    /**
+     * @param position the position to set
+     */
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    /**
+     * @return the midName
+     */
+    public String getMidName() {
+        return midName;
+    }
+
+    /**
+     * @param midName the midName to set
+     */
+    public void setMidName(String midName) {
+        this.midName = midName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the dateOfBirth
+     */
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /**
+     * @param dateOfBirth the dateOfBirth to set
+     */
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the manId
+     */
+    public int getManId() {
+        return manId;
+    }
+
+    /**
+     * @param manId the manId to set
+     */
+    public void setManId(int manId) {
+        this.manId = manId;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
