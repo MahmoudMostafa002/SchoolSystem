55d98d6fd12abc368e69c30cec8ecb14cbc15123
asd <asd@asd-PC>
2015-12-28 22:13:57 +0300

    Mahmoud

---
+++ SchoolManagmentSystem/src/com/school/dao/DataBaseDaoImp.java
@@ -0,0 +1,72 @@
+package com.school.dao;
+
+import com.school.pojo.DataBasePojo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DataBaseDaoImp implements DataBaseDao {
+
+    DataBasePojo mypojo;
+    PreparedStatement pstmt;
+    ResultSet rs;
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public Connection openConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String connectionUrl = "jdbc:mysql://localhost/School";
+            conn = DriverManager.getConnection(connectionUrl, "root", "123");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public void closeConnection(Connection conn) {
+        if (conn == null) {
+            return;
+        }
+        try {
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateQuery(String sql, Connection conn) {
+        if (conn == null) {
+            return;
+        }
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.executeUpdate(sql);
+        } catch (SQLException e) {
+            System.out.println("Duplicate Key please try new Key ");
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet excuteQuery(String sql, Connection conn) {
+        if (conn == null) {
+            return null;
+        }
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery(sql);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return rs;
+    }
+
+
+}
