790d5ae6685aa2a4b44579e6b0da62b24f0b797e
asd <asd@asd-PC>
2015-12-28 22:14:58 +0300

    Mahmoud

---
+++ SchoolManagmentSystem/src/com/school/pojo/DataBasePojo.java
@@ -0,0 +1,42 @@
+package com.school.pojo;
+
+import java.sql.*;
+public class DataBasePojo {
+  private Connection con;
+   private  ResultSet rs;    
+   private  PreparedStatement pstmt;   
+   private  Statement stmt;
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+
+    public ResultSet getRs() {
+        return rs;
+    }
+
+    public void setRs(ResultSet rs) {
+        this.rs = rs;
+    }
+
+    public PreparedStatement getPstmt() {
+        return pstmt;
+    }
+
+    public void setPstmt(PreparedStatement pstmt) {
+        this.pstmt = pstmt;
+    }
+
+    public Statement getStmt() {
+        return stmt;
+    }
+
+    public void setStmt(Statement stmt) {
+        this.stmt = stmt;
+    }
+      
+}
